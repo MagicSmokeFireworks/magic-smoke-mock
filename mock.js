0ad2af71785433a73df677d4c7904b0bdd3635b6
Brian Roth <viomonger@gmail.com>
2018-05-19 14:46:43 -0700

    Starting to build a page to control the mock board behavior.

--- mock.js
+++ mock.js
@@ -19,6 +19,7 @@ var boardinfo = JSON.parse(contents);
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
+	socket.emit('fresh state', boardState);
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	});
@@ -36,6 +37,7 @@ var boardName = boardinfo[boardKey].name;
 var port = process.argv[3];
 
 var sw_arm = '0';
+var hw_arm = 'ARMED';
 var command_count = 0;
 var fc0 = 0;
 var fc1 = 0;
@@ -70,6 +72,8 @@ const server = net.createServer((c) => {
 		console.log('data: ' + buf);
 
 		cmd = buf.toString('utf8');
+
+		// TODO ignore commands when powered off
 
 		if (cmd == 'identify') {
 			console.log('identify: ' + boardName);
@@ -113,6 +117,7 @@ const server = net.createServer((c) => {
 server.on('error', (err) => {
 	throw err;
 });
+
 
 var fire_channel = function(channel) {
 	console.log('firing channel ' + channel);
@@ -187,13 +192,80 @@ app.post('/fire*', function(req, res) {
 });
 */
 
+var boardState = {battery: 'connected', uplink: 'good', power_switch: 'on', arm_switch: 'armed'};
+
+var reset_board = function() {
+	sw_arm = '0';
+	command_count = 0;
+	fc0 = 0;
+	fc1 = 0;
+	fc2 = 0;
+	fc3 = 0;
+	fc4 = 0;
+	fc5 = 0;
+	fc6 = 0;
+	fc7 = 0;
+};
+
+
+app.post('/battery', function(req, res) {
+	var batterystate = req.query.state;
+	if (batterystate == 'connected') {
+		boardState.battery = 'connected';
+	}
+	else {
+		boardState.battery = 'disconnected';
+		reset_board();
+	}
+	res.end();
+	io.emit('fresh state', boardState);
+});
+
+app.post('/power', function(req, res) {
+	var powerstate = req.query.state;
+	if (powerstate == 'on') {
+		boardState.power_switch = 'on';
+	}
+	else {
+		boardState.power_switch = 'off';
+		reset_board();
+	}
+	res.end();
+	io.emit('fresh state', boardState);
+});
+
+app.post('/hwarm', function(req, res) {
+	var hwarmstate = req.query.state;
+	if (hwarmstate == 'arm') {
+		boardState.arm_switch = 'armed';
+		// TODO if arm sense working?
+		hw_arm = 'ARMED';
+	}
+	else {
+		boardState.arm_switch = 'disarmed';
+		// TODO if arm sense working?
+		hw_arm = 'DISARMED';
+	}
+	res.end();
+	io.emit('fresh state', boardState);
+});
+
 app.get('/', function(req, res) {
 	console.log('home');
-	res.render('home', {name: boardName});
-})
+	res.render('home', {name: boardName, state: boardState});
+});
 
 
 var sendTelem = setInterval(function() {
+
+	// do nothing if board not powered
+	if (boardState.battery != 'connected') {
+		return;
+	}
+	if (boardState.power_switch != 'on') {
+		return;
+	}
+
 
 	// object of options for the post
 	var post_options = {
@@ -208,7 +280,7 @@ var sendTelem = setInterval(function() {
 			'port': port,
 			'fver': 'MOCK',
 			'sw_arm': sw_arm,
-			'hw_arm': 'ARMED',
+			'hw_arm': hw_arm,
 			'wifi_rssi': '0',
 			'r0': m0,
 			'r1': m1,
@@ -236,6 +308,8 @@ var sendTelem = setInterval(function() {
 		res.on('data', function(chunk) {
 			console.log('Response: ' + chunk);
 		});
+	}).on('error', function(err) {
+		console.log("Error with connection to server: " + err);
 	});
 
 	// post the data
